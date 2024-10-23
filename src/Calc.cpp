2cd8eff84e1da48a8b7aa6f2568e2981673dfadf
sweatshirt0 <lil_6ix@aol.com>
2024-10-23 18:24:12 -0400

    calc.

---
+++ src/Calc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+  char op;
+  double num1;
+  double num2;
+  double result;
+
+  std::cout << "************CALCULATOR************\n";
+
+  std::cout << "Enter the operation you would like to do (+,-,*,/): ";
+
+  std::cin >> op;
+
+  std::cout << "Enter the first number: ";
+
+  std::cin >> num1;
+
+  std::cout >> "Enter your second number: ";
+
+  std::cin >> num2;
+
+  switch(op) {
+  case '+':
+    result = num1 + num2;
+    std::cout << "The addition of " << num1 << " and " << num2 << " is " << result << ".\n";
+    break;
+
+  case '-':
+    result = num1 - num2;
+    std::cout << "The subtraction of " << num1 << " and " << num2 << " is " << result << ".\n";
+    break;
+    
+  }
+
+  
+  std::cout << "**********************************";
+  
+
+  return 0;
+}
